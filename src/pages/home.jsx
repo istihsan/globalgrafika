35f525d5ce3f693a3c893a04c21f39b1bf6d7331
istihsan <istihsanbismo.putro@gmail.com>
2023-09-19 14:13:26 +0700

    Home v0.9

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-import heroImage from "../logo.svg";
-import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Footer from "./components/footer"
 import ProductCard from "./components/productcard"
 import FrequentlyAskedQuestion from "./components/frequentlyaskedquestions";
-import { AbsoluteCenter } from "@chakra-ui/react";
 import ProductCardH from "./components/productcardh";
 
 const Home = () => {
