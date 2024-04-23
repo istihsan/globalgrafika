20e7ba7aed474ce5b6d64899301e67ce6364c3b7
istihsan <istihsanbismo.putro@gmail.com>
2024-04-24 00:27:44 +0700

    Test orderController

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -7,7 +7,7 @@ const storage = new Storage({
   projectId: "big-oxygen-413514",
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    private_key: process.env.GOOGLE_PRIVATE_KEY
   }
 });
 const bucket = storage.bucket("globalgrafikabucket");
